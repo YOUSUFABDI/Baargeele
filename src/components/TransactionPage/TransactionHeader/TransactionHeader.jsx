18ece9119dbb32d4faf5dc98bcb47649e41cc3d4
Ahmed Khaliif <ahmedkhaliif@Ahmeds-MacBook-Pro.local>
2023-02-26 14:54:21 +0300

    tran header

--- src/components/TransactionPage/TransactionHeader/TransactionHeader.jsx
+++ src/components/TransactionPage/TransactionHeader/TransactionHeader.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const TransactionHeader = () => {
+  const loction = useLocation();
+  const user = location.state.user;
   return (
     <div className="transaction__header">
       <div className="transaction__banner">
         <Link to="/" className="transaction__logo">
           Baargeele
         </Link>
-        <span className="transaction__name">Yuusuf</span>
+        <span className="transaction__name">{user.Name}</span>
       </div>
       <div className="transaction__border"></div>
       <div className="transaction__title">
