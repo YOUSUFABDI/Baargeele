2a35ee598c6e7043b1f8a8498a50c2d587bda798
unknown <yufis0844@gmail.com>
2023-03-11 20:57:02 +0300

    added contact page

--- src/components/ContactPage/Contact.jsx
+++ src/components/ContactPage/Contact.jsx
@@ -1,5 +1,9 @@
 import Header from "../Header/Header";
 import "./contact.css";
+import getInTouchImg from "../../../assets/images/Get-in-touch.svg";
+import { BsWhatsapp } from "react-icons/bs";
+import { BsFilePerson } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Contact = () => {
   return (
@@ -8,8 +12,46 @@ const Contact = () => {
         <Header />
       </nav>
 
-      <div>
-        <h1 className="contact">Contact page</h1>
+      <div className="contact__wrapper">
+        <div className="contact__img__info">
+          <div className="image__info">
+            <img
+              className="contact__img"
+              src={getInTouchImg}
+              alt="get-in-touch-img"
+            />
+            <div className="info__wrapper">
+              <div className="what__up__icon">
+                <p className="info__wrappe__desc">Let's Talk</p>
+                <BsWhatsapp className="info__wrappe__icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="get__in__touch__wrapper">
+          <h2>Get in touch</h2>
+          <form className="get__in__touch__form">
+            <div className="get__in__touch__inpu__wrapper">
+              <BsFilePerson className="get__in__touch__icon" size={20} />
+              <input type="text" placeholder="Enter Your Name" />
+            </div>
+            <div className="get__in__touch__inpu__wrapper">
+              <MdOutlineEmail className="get__in__touch__icon" size={20} />
+              <input type="text" placeholder="Enter Gmail" />
+            </div>
+            <div className="get__in__touch__text__input__wrapper">
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <button className="info__wrapper__btn">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
